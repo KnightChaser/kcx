0ef169bf6aaf56962e39f8a09df174f28bb7bee8
Lee Garam <agerio100@naver.com>
2024-05-11 02:54:45 +0900

    Decorating welcome page
    With glittering aura glow page will welcome the user.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,21 +1,32 @@
 <script>
-    import Router from 'svelte-spa-router';
+    import Router, { location } from 'svelte-spa-router';
     import Header from '../routes/header.svelte';
+    import Welcome from '../routes/welcome.svelte';
     import Login from '../routes/login.svelte';
     import Logout from '../routes/logout.svelte';
     // import Register from '../routes/register.svelte';
     import 'bootstrap/dist/css/bootstrap.min.css';
     import 'bootstrap/dist/js/bootstrap.min.js';
 
+    import { auth } from '../src/stores/auth.js'
+
     const routes = {
+        '/': Welcome,  // Ensure there's a route for '/'
         '/login': Login,
         '/logout': Logout
+        // '/register': Register
     };
 </script>
 
 <main>
     <Header />
     <Router {routes} />
+    <!-- Reactively show Welcome component -->
+    <body>
+    {#if !$auth && $location === '/'}
+        <Welcome />
+    {/if}
+    </body>
 </main>
 
 <style>

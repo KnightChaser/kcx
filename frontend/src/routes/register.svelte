8058e7e1eba1059825aa9eae30894e0d5f2e65f8
Lee Garam <agerio100@naver.com>
2024-05-12 13:34:14 +0900

    Change register page style to TailwindCSS

--- frontend/src/routes/register.svelte
+++ frontend/src/routes/register.svelte
@@ -21,25 +21,14 @@
             });
             return;
         }
-        if (!acceptTerms) {
-            Swal.fire({
-                title: "Error",
-                text: "Accept the terms & condition to proceed.",
-                icon: "error",
-            });
-            return;
-        }
 
-        const response = await fetch(
-            `${BACKEND_API_URL}/account/register`,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ username, email, password }),
-            }
-        );
+        const response = await fetch(`${BACKEND_API_URL}/account/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, email, password }),
+        });
 
         if (!response.ok) {
             const data = await response.json();
@@ -65,92 +54,91 @@
             }
         });
     };
+
+    // Accordion toggle
+    let isAccordionOpen = false;
+    function toggleAccordion() {
+        isAccordionOpen = !isAccordionOpen;
+    }
 </script>
 
-<main class="container mt-5" id="registration_form">
-    <h2>Register</h2>
+<main class="container mx-auto px-4 mt-8" id="registration_form">
+    <h2 class="text-3xl font-semibold mb-6 py-2">Register</h2>
     <form on:submit|preventDefault={submitForm}>
-        <div class="mb-3">
-            <label for="username" class="form-label">ID</label>
+        <div class="mb-4">
+            <label
+                for="username"
+                class="block text-sm font-medium text-gray-700">ID</label
+            >
             <input
                 type="text"
-                class="form-control"
+                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 id="username"
                 bind:value={username}
                 required
             />
         </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
+        <div class="mb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700"
+                >Email</label
+            >
             <input
                 type="email"
-                class="form-control"
+                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 id="email"
                 bind:value={email}
                 required
             />
         </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
+        <div class="mb-4">
+            <label
+                for="password"
+                class="block text-sm font-medium text-gray-700">Password</label
+            >
             <input
                 type="password"
-                class="form-control"
+                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 id="password"
                 bind:value={password}
                 required
             />
         </div>
-        <div class="mb-3">
-            <label for="confirmPassword" class="form-label"
+        <div class="mb-4">
+            <label
+                for="confirmPassword"
+                class="block text-sm font-medium text-gray-700"
                 >Confirm Password</label
             >
             <input
                 type="password"
-                class="form-control"
+                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                 id="confirmPassword"
                 bind:value={confirmPassword}
                 required
             />
         </div>
-        <div class="accordion" id="accordionPanelsStayOpenExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button
-                        class="accordion-button"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapseOne"
-                        aria-expanded="true"
-                        aria-controls="panelsStayOpen-collapseOne"
-                    >
-                        Terms & Conditions
-                    </button>
-                </h2>
-                <div
-                    id="panelsStayOpen-collapseOne"
-                    class="accordion-collapse collapse show"
-                >
-                    <div class="accordion-body">
-                        By clicking the "Register" button, you agree to this condition.<br>
-                        <strong>First</strong>, any abusing or inappropriate behavior is strictly prohibited.<br>
-                        <strong>Second</strong>, you must not share your account information with others.<br>
-                        <strong>Third</strong>, you have not to leak any security vulnerabilities to the public(If you find any, please report it to the administrator immediately.)
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mb-3 form-check">
-            <input
-                type="checkbox"
-                class="form-check-input"
-                id="acceptTerms"
-                bind:checked={acceptTerms}
-            />
-            <label class="form-check-label" for="acceptTerms"
-                >I accept the terms and conditions</label
+        <div class="my-4">
+            <button
+                type="button"
+                class="text-left w-full py-2 px-4 bg-gray-100 text-gray-800 font-medium rounded-md"
+                on:click={toggleAccordion}
             >
+                You agree to the terms and conditions by clicking the "Register" button. (Click to expand)
+            </button>
+            {#if isAccordionOpen}
+                <div class="border p-4 mt-2 rounded-md">
+                    By clicking the "Register" button, you agree to this condition.<br />
+                    <strong>First</strong>, any abusing or inappropriate behavior is strictly prohibited.<br />
+                    <strong>Second</strong>, you must not share your account information with others.<br />
+                    <strong>Third</strong>, you have not to leak any security vulnerabilities to the public (If you find any, please report it to the administrator immediately.)
+                </div>
+            {/if}
         </div>
-        <button type="submit" class="btn btn-primary">Register</button>
+        <!-- register button -->
+        <button
+            type="submit"
+            class="w-full py-2 px-4 bg-indigo-600 text-white font-medium rounded-md"
+        >Register</button>
     </form>
 </main>
 

9eb30306acb63a012807f6855ce262ba9faab235
Lee Garam <agerio100@naver.com>
2024-05-18 01:09:56 +0900

    Fix decimal point limit according to the price

--- frontend/src/utils/formatCurrency.js
+++ frontend/src/utils/formatCurrency.js
@@ -1,3 +1,13 @@
 export function formatCurrency(value) {
-    return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(value);
+    if (value >= 1000)
+        return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(value);
+    if (value >= 100)
+        // With one decimal places
+        return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(value);
+    if (value >= 1)
+        // With two decimal places
+        return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(value);
+    else
+        // With four decimal places
+        return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW', minimumFractionDigits: 4, maximumFractionDigits: 4 }).format(value);
 }

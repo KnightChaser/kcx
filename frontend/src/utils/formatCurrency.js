49dd1582b3e6c094de6e68d9248572d4ebed29e7
Lee Garam <agerio100@naver.com>
2024-05-23 00:49:06 +0900

    Fix formatCurrency that negative values weren't affected

--- frontend/src/utils/formatCurrency.js
+++ frontend/src/utils/formatCurrency.js
@@ -1,10 +1,11 @@
 export function formatCurrency(value) {
-    if (value >= 1000)
+    if (Math.abs(value) >= 1000)
+        // Without decimal places
         return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(value);
-    if (value >= 100)
+    if (Math.abs(value) >= 100)
         // With one decimal places
         return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW', minimumFractionDigits: 1, maximumFractionDigits: 1 }).format(value);
-    if (value >= 1)
+    if (Math.abs(value) >= 1)
         // With two decimal places
         return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(value);
     else

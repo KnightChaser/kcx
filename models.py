a9a3c5233f84415c91be5932d4838993bf4d8354
Lee Garam <agerio100@naver.com>
2024-05-31 22:56:18 +0900

    Added more crypto markets
    Newly added AAVE, BAT, BTG, BTT, EOS, MANA, NEO, ONT, QTUM, SAND, SBD, SC, STORJ, STRAX, SUI, TFUEL, THETA, VET, WAVES, XEM, XLM, XTZ

--- models.py
+++ models.py
@@ -20,39 +20,84 @@ class Balance(Base):
     id                      = Column(Integer, primary_key=True, index=True)
     user_id                 = Column(Integer, ForeignKey("users.id"))
     KRW                     = Column(Float, default=0)        # South Korean Won (fiat currency)
+    AAVE                    = Column(Float, default=0)        # Aave
     ADA                     = Column(Float, default=0)        # Cardano
     AVAX                    = Column(Float, default=0)        # Avalanche
+    BAT                     = Column(Float, default=0)        # Basic Attention Token
     BCH                     = Column(Float, default=0)        # Bitcoin Cash
     BTC                     = Column(Float, default=0)        # Bitcoin
+    BTG                     = Column(Float, default=0)        # Bitcoin Gold
+    BTT                     = Column(Float, default=0)        # BitTorrent
     DOT                     = Column(Float, default=0)        # Polkadot
+    EOS                     = Column(Float, default=0)        # EOS
     ETC                     = Column(Float, default=0)        # Ethereum Classic
     ETH                     = Column(Float, default=0)        # Ethereum
     HBAR                    = Column(Float, default=0)        # Hedera Hashgraph
     LINK                    = Column(Float, default=0)        # Chainlink
+    MANA                    = Column(Float, default=0)        # Decentraland
     MATIC                   = Column(Float, default=0)        # Polygon
     NEAR                    = Column(Float, default=0)        # NEAR Protocol
+    NEO                     = Column(Float, default=0)        # NEO
+    ONT                     = Column(Float, default=0)        # Ontology
+    QTUM                    = Column(Float, default=0)        # Quantum
+    SAND                    = Column(Float, default=0)        # Sandbox
+    SBD                     = Column(Float, default=0)        # Steem Dollars
+    SC                      = Column(Float, default=0)        # Siacoin
     SHIB                    = Column(Float, default=0)        # Shiba Inu
     SOL                     = Column(Float, default=0)        # Solana
+    STORJ                   = Column(Float, default=0)        # Storage
+    STRAX                   = Column(Float, default=0)        # Stratis
+    SUI                     = Column(Float, default=0)        # SUI
+    TFUEL                   = Column(Float, default=0)        # Theta Fuel
+    THETA                   = Column(Float, default=0)        # Theta Token
     TRX                     = Column(Float, default=0)        # TRON
+    VET                     = Column(Float, default=0)        # VeChain
+    WAVES                   = Column(Float, default=0)        # Waves
     XEC                     = Column(Float, default=0)        # eCash
+    XEM                     = Column(Float, default=0)        # NEM
+    XLM                     = Column(Float, default=0)        # Stellar Lumens
     XRP                     = Column(Float, default=0)        # Ripple
+    XTZ                     = Column(Float, default=0)        # Tezos
 
-    ADA_average_unit_price  = Column(Float, default=0)        # Average unit price of ADA(Cardano)
-    AVAX_average_unit_price = Column(Float, default=0)        # Average unit price of AVAX(Avalanche)
-    BCH_average_unit_price  = Column(Float, default=0)        # Average unit price of BCH(Bitcoin Cash)
-    BTC_average_unit_price  = Column(Float, default=0)        # Average unit price of BTC(Bitcoin)
-    DOT_average_unit_price  = Column(Float, default=0)        # Average unit price of DOT(Polkadot)
-    ETC_average_unit_price  = Column(Float, default=0)        # Average unit price of ETC(Ethereum Classic)
-    ETH_average_unit_price  = Column(Float, default=0)        # Average unit price of ETH(Ethereum)
-    HBAR_average_unit_price = Column(Float, default=0)        # Average unit price of HBAR(Hedera Hashgraph)
-    LINK_average_unit_price = Column(Float, default=0)        # Average unit price of LINK(Chainlink)
-    MATIC_average_unit_price= Column(Float, default=0)        # Average unit price of MATIC(Polygon)
-    NEAR_average_unit_price = Column(Float, default=0)        # Average unit price of NEAR(NEAR Protocol)
-    SHIB_average_unit_price = Column(Float, default=0)        # Average unit price of SHIB(Shiba Inu)
-    SOL_average_unit_price  = Column(Float, default=0)        # Average unit price of SOL(Solana)
-    TRX_average_unit_price  = Column(Float, default=0)        # Average unit price of TRX(TRON)
-    XEC_average_unit_price  = Column(Float, default=0)        # Average unit price of XEC(eCash)
-    XRP_average_unit_price  = Column(Float, default=0)        # Average unit price of XRP(Ripple)
+
+    AAVE_average_unit_price     = Column(Float, default=0)    # Average unit price of AAVE(Aave)
+    ADA_average_unit_price      = Column(Float, default=0)    # Average unit price of ADA(Cardano)
+    AVAX_average_unit_price     = Column(Float, default=0)    # Average unit price of AVAX(Avalanche)
+    BAT_average_unit_price      = Column(Float, default=0)    # Average unit price of BAT(Basic Attention Token)
+    BCH_average_unit_price      = Column(Float, default=0)    # Average unit price of BCH(Bitcoin Cash)
+    BTC_average_unit_price      = Column(Float, default=0)    # Average unit price of BTC(Bitcoin)
+    BTG_average_unit_price      = Column(Float, default=0)    # Average unit price of BTG(Bitcoin Gold)
+    BTT_average_unit_price      = Column(Float, default=0)    # Average unit price of BTT(BitTorrent)
+    DOT_average_unit_price      = Column(Float, default=0)    # Average unit price of DOT(Polkadot)
+    EOS_average_unit_price      = Column(Float, default=0)    # Average unit price of EOS
+    ETC_average_unit_price      = Column(Float, default=0)    # Average unit price of ETC(Ethereum Classic)
+    ETH_average_unit_price      = Column(Float, default=0)    # Average unit price of ETH(Ethereum)
+    HBAR_average_unit_price     = Column(Float, default=0)    # Average unit price of HBAR(Hedera Hashgraph)
+    LINK_average_unit_price     = Column(Float, default=0)    # Average unit price of LINK(Chainlink)
+    MANA_average_unit_price     = Column(Float, default=0)    # Average unit price of MANA(Decentraland)
+    MATIC_average_unit_price    = Column(Float, default=0)    # Average unit price of MATIC(Polygon)
+    NEAR_average_unit_price     = Column(Float, default=0)    # Average unit price of NEAR(NEAR Protocol)
+    NEO_average_unit_price      = Column(Float, default=0)    # Average unit price of NEO
+    ONT_average_unit_price      = Column(Float, default=0)    # Average unit price of ONT(Ontology)
+    QTUM_average_unit_price     = Column(Float, default=0)    # Average unit price of QTUM(Quantum)
+    SAND_average_unit_price     = Column(Float, default=0)    # Average unit price of SAND(Sandbox)
+    SBD_average_unit_price      = Column(Float, default=0)    # Average unit price of SBD(Steem Dollars)
+    SC_average_unit_price       = Column(Float, default=0)    # Average unit price of SC(Siacoin)
+    SHIB_average_unit_price     = Column(Float, default=0)    # Average unit price of SHIB(Shiba Inu)
+    SOL_average_unit_price      = Column(Float, default=0)    # Average unit price of SOL(Solana)
+    STORJ_average_unit_price    = Column(Float, default=0)    # Average unit price of STORJ(Storage)
+    STRAX_average_unit_price    = Column(Float, default=0)    # Average unit price of STRAX(Stratis)
+    SUI_average_unit_price      = Column(Float, default=0)    # Average unit price of SUI
+    TFUEL_average_unit_price    = Column(Float, default=0)    # Average unit price of TFUEL(Theta Fuel)
+    THETA_average_unit_price    = Column(Float, default=0)    # Average unit price of THETA(Theta Token)
+    TRX_average_unit_price      = Column(Float, default=0)    # Average unit price of TRX(TRON)
+    VET_average_unit_price      = Column(Float, default=0)    # Average unit price of VET(VeChain)
+    WAVES_average_unit_price    = Column(Float, default=0)    # Average unit price of WAVES
+    XEC_average_unit_price      = Column(Float, default=0)    # Average unit price of XEC(eCash)
+    XEM_average_unit_price      = Column(Float, default=0)    # Average unit price of XEM(NEM)
+    XLM_average_unit_price      = Column(Float, default=0)    # Average unit price of XLM(Stellar Lumens)
+    XRP_average_unit_price      = Column(Float, default=0)    # Average unit price of XRP(Ripple)
+    XTZ_average_unit_price      = Column(Float, default=0)    # Average unit price of XTZ(Tezos)
 
     created_at              = Column(DateTime(timezone=True), server_default=func.now())
 
--- schemas.py
+++ schemas.py
@@ -17,37 +17,83 @@ class LoginSchema(BaseModel):
 # User balance schema
 class BalanceSchema(BaseModel):
     KRW: FiniteFloat
+    AAVE: FiniteFloat
     ADA: FiniteFloat
     AVAX: FiniteFloat
+    BAT: FiniteFloat
     BCH: FiniteFloat
     BTC: FiniteFloat
+    BTG: FiniteFloat
+    BTT: FiniteFloat
     DOT: FiniteFloat
+    EOS: FiniteFloat
     ETC: FiniteFloat
+    ETH: FiniteFloat
     HBAR: FiniteFloat
     LINK: FiniteFloat
+    MANA: FiniteFloat
     MATIC: FiniteFloat
     NEAR: FiniteFloat
+    NEO: FiniteFloat
+    ONT: FiniteFloat
+    QTUM: FiniteFloat
+    SAND: FiniteFloat
+    SBD: FiniteFloat
+    SC: FiniteFloat
     SHIB: FiniteFloat
     SOL: FiniteFloat
+    STORJ: FiniteFloat
+    STRAX: FiniteFloat
+    SUI: FiniteFloat
+    TFUEL: FiniteFloat
+    THETA: FiniteFloat
     TRX: FiniteFloat
+    VET: FiniteFloat
+    WAVES: FiniteFloat
     XEC: FiniteFloat
+    XEM: FiniteFloat
+    XLM: FiniteFloat
     XRP: FiniteFloat
+    XTZ: FiniteFloat
 
+    AAVE_average_unit_price: FiniteFloat
     ADA_average_unit_price: FiniteFloat
     AVAX_average_unit_price: FiniteFloat
+    BAT_average_unit_price: FiniteFloat
     BCH_average_unit_price: FiniteFloat
     BTC_average_unit_price: FiniteFloat
+    BTG_average_unit_price: FiniteFloat
+    BTT_average_unit_price: FiniteFloat
     DOT_average_unit_price: FiniteFloat
+    EOS_average_unit_price: FiniteFloat
     ETC_average_unit_price: FiniteFloat
+    ETH_average_unit_price: FiniteFloat
     HBAR_average_unit_price: FiniteFloat
     LINK_average_unit_price: FiniteFloat
+    MANA_average_unit_price: FiniteFloat
     MATIC_average_unit_price: FiniteFloat
     NEAR_average_unit_price: FiniteFloat
+    NEO_average_unit_price: FiniteFloat
+    ONT_average_unit_price: FiniteFloat
+    QTUM_average_unit_price: FiniteFloat
+    SAND_average_unit_price: FiniteFloat
+    SBD_average_unit_price: FiniteFloat
+    SC_average_unit_price: FiniteFloat
     SHIB_average_unit_price: FiniteFloat
     SOL_average_unit_price: FiniteFloat
+    STORJ_average_unit_price: FiniteFloat
+    STRAX_average_unit_price: FiniteFloat
+    SUI_average_unit_price: FiniteFloat
+    TFUEL_average_unit_price: FiniteFloat
+    THETA_average_unit_price: FiniteFloat
     TRX_average_unit_price: FiniteFloat
+    VET_average_unit_price: FiniteFloat
+    WAVES_average_unit_price: FiniteFloat
     XEC_average_unit_price: FiniteFloat
+    XEM_average_unit_price: FiniteFloat
+    XLM_average_unit_price: FiniteFloat
     XRP_average_unit_price: FiniteFloat
+    XTZ_average_unit_price: FiniteFloat
 
 # User balance schema for deposit and withdraw
 class BalanceDepositWithdrawSchema(BaseModel):

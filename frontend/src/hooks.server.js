f349861e689a4585a776d094846c504d421aed15
Lee Garam <agerio100@naver.com>
2024-06-23 22:25:56 +0900

    Fix server hook for route protection handling URLs
    Because Sveltekit is based on server rendering... using browser($app/environment) is encouraged

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -1,15 +1,18 @@
-// src/hooks.server.js
+// frontend/src/hooks.server.js
+import { browser } from '$app/environment';
 
 export async function handle({ event, resolve }) {
     // Check authentication for protected routes
-    const protectedRoutes = ['/dashboards', '/user', '/exchange'];
+    const protectedRoutes = ['/dashboards', '/accounts/logout', '/accounts/user', '/exchange'];
     const isProtected = protectedRoutes.some(route => event.url.pathname.startsWith(route));
 
     // Check if the authentication cookie is present
     const isAuthenticated = event.request.headers.get('cookie')?.includes('access_token');
 
     if (isProtected && !isAuthenticated) {
-        return Response.redirect('/accounts/login');
+        if (browser) {
+            window.location.href = '/accounts/login';
+        }
     }
 
     return resolve(event);

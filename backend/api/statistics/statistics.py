3bb80cfd9ea564fefda85a3dfcd1f3200e2028cb
Lee Garam <agerio100@naver.com>
2024-07-06 18:05:34 +0900

    Get the number of transactions made

--- backend/api/statistics/statistics.py
+++ backend/api/statistics/statistics.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from database_session import get_sqlite3_db, get_redis_db
-from models import Statistics
+from models import Statistics, TradeHistory
 import json
 
 router = APIRouter()
@@ -22,6 +22,16 @@ async def get_total_transactions(db: Session = Depends(get_sqlite3_db)) -> JSONR
             "KRW": statistics.total_transaction_amount
         }
         return JSONResponse(content=total_transactions, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+    
+# Get the number of transactions made
+@router.get("/api/statistics/total-transaction-count")
+async def get_total_transaction_count(db: Session = Depends(get_sqlite3_db)) -> JSONResponse:
+    try:
+        # Get the number of rows in the transactions table
+        total_transaction_count = db.query(TradeHistory).count()
+        return JSONResponse(content={"count": total_transaction_count}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
--- frontend/src/routes/welcome/totalTransactionAmountPanel.svelte
+++ frontend/src/routes/welcome/totalTransactionAmountPanel.svelte
@@ -6,8 +6,8 @@
     import axios from 'axios';
     import { writable } from 'svelte/store';
 
-    // Create a writable store to hold the transaction amount
     let totalTransactionAmount = writable(0);
+    let totalTransactionCount = writable(0);
 
     const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
 
@@ -24,6 +24,17 @@
         }
     }
 
+    // Function to get the total transaction count
+    async function fetchTotalTransactionCount() {
+        try {
+            const response = await axios.get(`${BACKEND_API_URL}/statistics/total-transaction-count`);
+            // Access the count value from the response data
+            totalTransactionCount.set(response.data.count);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     // Fetch the total transaction amount every 1 second
     onMount(() => {
         // Immediately fetch data once
@@ -31,6 +42,7 @@
 
         fetchInterval = setInterval(() => {
             fetchTotalTransactionAmount();
+            fetchTotalTransactionCount();
         }, 1000);
     });
 
@@ -41,10 +53,11 @@
 
 <!-- Display the total transaction amount with styling -->
 <div id="totalTransactionAmountPanel" class="bg-white shadow-lg rounded-lg px-6 py-8 m-4 w-full text-center">
-    <p class="text-2xl font-semibold text-gray-700">Total transactions so far</p>
+    <p class="text-2xl font-semibold text-gray-700">Members have traded</p>
     <p class="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-purple-600 mt-4">
         <span>{Math.round($totalTransactionAmount).toLocaleString()} KRW</span>
     </p>
+    <p class="text-lg text-gray-500 mt-2">for <strong>{($totalTransactionCount).toLocaleString()}</strong> transactions so far</p>
 </div>
 
 <style>

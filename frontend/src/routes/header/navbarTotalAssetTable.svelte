4ee8c3f9eea5017a1852cf830a5369c06aaea95c
Lee Garam <agerio100@naver.com>
2024-07-06 21:39:27 +0900

    Change navbar asset table font for fixed width

--- frontend/src/routes/header/navbarTotalAssetTable.svelte
+++ frontend/src/routes/header/navbarTotalAssetTable.svelte
@@ -56,9 +56,9 @@
     });
 </script>
 
-<div class="bg-white rounded-lg shadow pl-2 pr-2 py-2 flex flex-col items-center">
-    <p class="text-gray-700 text-sm mb-1">Total Asset Value</p>
-    <div class="text-gray-800 text-lg font-semibold xl">
+<div class="bg-white rounded-lg shadow pl-2 pr-2 py-2 flex flex-col items-center" id="navbar_total_asset_table">
+    <p class="text-gray-700 text-sm mb-1 font-bold">Total Asset Value</p>
+    <div class="text-gray-800 text-lg font-extrabold xl">
         ₩{Math.round($localTotalKRW).toLocaleString()}
     </div>
 </div>
@@ -69,5 +69,9 @@
     }
     .shadow {
         box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    }
+
+    #navbar_total_asset_table {
+        font-family: "SF Mono", "Fira Code", "Fira Mono", "Roboto Mono", monospace;
     }
 </style>
